48430e3b286233a6a97b02c3e564904e870be1a4
SmokierLemur51 <ldl6147@gmail.com>
2023-07-14 17:32:46 -0400

    made some changs ...

--- main.go
+++ main.go
@@ -2,30 +2,72 @@ package main
 
 import (
 	"net/http"
-	"html/template"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
 )
 
+const (
+	TITLE = "Greenleaf Cleaning"
+)
 
-func main() {
-	// router
-	r := chi.NewRouter()
-	
-	// init templates
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+func main(){
+	r := gin.Default()
 
-	// middleware
-	r.Use(middleware.Logger)
-	
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		err := tmpl.Execute(w, nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": TITLE,
+		})
 	})
 
-	http.ListenAndServe(":5000", r)
+	r.GET("/about", aboutHandler)
+
+	r.GET("/contact", contactHandler)
+
+	r.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	// setup route group for the api
+	api := r.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H {
+				"message": "pong",
+			})
+		})
+	}
+
+	r.Run(":5000")
+}
+
+type ContactInformation struct {
+	Name			string	`json:"name"`
+	Email			string	`json:"email"`
+	PhoneNumber		string	`json:"phonenumber"`
+}
+
+
+func contactHandler(c *gin.Context) {
+	var contact ContactInformation 
+
+	if err := c.ShouldBindJSON(&contact); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Thank you, we will be in touch!",
+		"contact": contact, 
+	})
+}
+
+
+func aboutHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "about.html", gin.H{
+		"page": "About",
+		"title": TITLE,	
+	})
 }
