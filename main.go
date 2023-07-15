5271ed44a50a10c8dad9c50d526292d794a0aef4
SmokierLemur51 <ldl6147@gmail.com>
2023-07-14 20:19:11 -0400

    updates

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	
+	"./routes"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
@@ -21,10 +23,12 @@ func main(){
 			"title": TITLE,
 		})
 	})
+	
+	// routes
+	r.GET("/about", rotues.aboutHandler)
+	r.GET("/contact", routes.contactHandler)
+	r.GET("/testing", routes.testHandler)
 
-	r.GET("/about", aboutHandler)
-
-	r.GET("/contact", contactHandler)
 
 	r.Use(static.Serve("/", static.LocalFile("./views", true)))
 
@@ -41,33 +45,4 @@ func main(){
 	r.Run(":5000")
 }
 
-type ContactInformation struct {
-	Name			string	`json:"name"`
-	Email			string	`json:"email"`
-	PhoneNumber		string	`json:"phonenumber"`
-}
 
-
-func contactHandler(c *gin.Context) {
-	var contact ContactInformation 
-
-	if err := c.ShouldBindJSON(&contact); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Thank you, we will be in touch!",
-		"contact": contact, 
-	})
-}
-
-
-func aboutHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "about.html", gin.H{
-		"page": "About",
-		"title": TITLE,	
-	})
-}
